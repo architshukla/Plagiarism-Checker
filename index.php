f0c5b677564908fbd2e1eb9896e2b7b893d3b201
Archit Shukla <architshukla2@gmail.com>
2013-09-03 23:25:34 +0530

    Added index.php: hero unit added with help modal

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html lang="en" xml:lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta content="text/html; charset=utf-8" http-equiv="content-type" />
+	<title> Plagiarism Checker </title>
+	<link href = "assets/css/bootstrap.css" rel="stylesheet" media="screen">
+	<script src = "assets/js/jquery.js"></script>
+	<script src = "assets/js/bootstrap.min.js"></script>
+	<style>
+		input:focus 
+		{
+    		outline:0px !important;
+    		-webkit-appearance:none;
+        }
+        a:focus 
+		{
+    		outline:0px !important;
+    		-webkit-appearance:none;
+        }
+        button:focus 
+		{
+    		outline:0px !important;
+    		-webkit-appearance:none;
+        }
+	</style>
+</head>
+<body>
+	<div class='hero-unit'>
+		<h1> Plagiarism Checker </h1>
+		<h1><small>An online utility to check if a document's contents are plagiarsed. </small></h1>
+		<p> </p>
+		<a class='btn btn-danger btn-large' data-toggle='modal' href='#myModal'> Learn More </a>
+	</div> 
+
+	<div id='myModal' class="modal hide fade">
+    	<div class="modal-header">
+    		<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+    		<h3>Plagiarism Checker - User Guide</h3>
+    	</div>
+    	<div class="modal-body">
+    	<!-- TODO: Add Help -->
+    		<p>Usage</p>
+    	</div>
+    	<div class="modal-footer">
+    		<a href="#" data-dismiss="modal" class="btn btn-primary">Close</a>
+    	</div>
+    </div>
+</body>
+</html>
